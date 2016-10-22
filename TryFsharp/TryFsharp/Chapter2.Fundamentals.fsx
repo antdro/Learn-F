4f93be0ee2d1f024d31196ed19c1b7d9882c37c2
Anton Drozdov <anton_drozdov@yahoo.co.uk>
2016-10-22 22:15:29 +0100

    update: Chapter2 Fundamentals - Core Types
    More examples added to Core Types
    Core Types is now complete

--- TryFsharp/TryFsharp/Chapter2.Fundamentals.fsx
+++ TryFsharp/TryFsharp/Chapter2.Fundamentals.fsx
@@ -1,5 +1,4 @@
 ﻿// CORE TYPES
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 // Unit
 //****************************************************************************
@@ -141,3 +140,105 @@ else
 // filter function
 let isGreater30 x = (x > 30)
 printfn "elements greater than 30: %A" (List.filter isGreater30 l1)
+
+
+// Aggregate operators
+//****************************************************************************
+
+//List.map
+//****************************************************************************
+// ('a -> 'b) -> 'a list -> 'b list
+let power x = x**3.0
+let list4 = [1.0..5.00]
+printfn "list: %A\npowered list: %A" list4 (List.map power list4)
+
+//List.fold
+//****************************************************************************
+// there are two types of folds: list.reduce and list.fold
+
+// List.reduce 
+// iterates through each element of a list and builds an accumulator value
+// accumulator value - summary of the processing done on the list so far
+// type ('a -> 'a -> 'a) -> 'a list -> 'a
+
+// building summation expression from a list of numbers
+let sumOfTwoNumbers x y = x + "+" + y
+let listOfNumbers = ["1";"33";"21";"56";"9";"10"]
+
+printfn "list of values: %A\nexpression: %A" 
+    listOfNumbers (List.reduce sumOfTwoNumbers listOfNumbers)
+
+// List.fold
+// allows to build a custom accumulator type
+// ('acc -> 'b -> 'acc) -> 'acc -> 'b list -> 'acc
+
+// count vowels in a string
+let str = "Fargo is one of the best mvoies I have ever watched"
+let countVowels (str:string) =    
+    let charList = List.ofSeq str
+    let funAcc (As, Es, Is, Os, Us) letter =
+        match letter with
+        | 'a' -> (As + 1, Es, Is, Os, Us)
+        | 'e' -> (As, Es + 1, Is, Os, Us)
+        | 'i' -> (As, Es, Is + 1, Os, Us)
+        | 'o' -> (As, Es, Is, Os + 1, Us)
+        | 'u' -> (As, Es, Is, Os, Us + 1)
+        |  _  -> (As, Es, Is, Os, Us)
+    List.fold funAcc (0,0,0,0,0) charList
+
+printfn "string: %s\nvowels count: %A" str (countVowels str)
+
+
+// List.iter
+//****************************************************************************
+// usually used to handle side effects
+// ('a -> unit) -> 'a list -> unit
+
+// print numbers from a list
+let printNumbers x = printfn "printing %d" x
+let list5 = [1..6]
+
+List.iter printNumbers list5
+
+
+
+// Option
+//****************************************************************************
+// option type is used to represent a value that may or may not exist
+// has only two values: Some('a) and None
+
+// retrieve the value of an option type
+// print negative values from a list
+let list6 = [for i in -10..10 -> i + 3 ]
+let isNegative x = (x < 0)
+let containsNegative list = 
+    let filteredList = List.filter isNegative list
+    if List.length filteredList > 0 
+    then Some(filteredList)
+    else None
+
+if Option.isSome (containsNegative list6) then
+    printfn "list: %A\nnegative values: %A"
+        list6 (Option.get (containsNegative list6))
+else printfn "no negative values were found"
+    
+
+//Printfn
+//****************************************************************************
+
+//printf
+// writes to the screen without adding a line continuation
+printf "printf: Hi, this text will not end up with a line continuation, "
+printf "printf: \\n will be added with new printf statement\n"
+
+//printfn
+// writes to the screen with adding a line continuation
+printfn "printfn: Hi, this text will end up with a line continuation"
+printfn "printfn: New statement is on a new line as printfn ends with \\n"
+
+//sprintf
+// returns string, but not unit type
+let s = sprintf "sprintf: this statement will be put in a string s"
+printfn "printfn: '%s'" s
+
+
